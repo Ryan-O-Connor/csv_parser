b6fab963ba505d30c50399724b7cdcb2a4604b48
Ryan-O-Connor <74995807+Ryan-O-Connor@users.noreply.github.com>
2021-06-02 07:27:12 -0500

    Refactor

--- csv.c
+++ csv.c
@@ -1,137 +1,113 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "csv.h"
 
 
-static int MAXBUFFER = 1000;
-static int MAXFIELDS = 100;
-static char delimiters[] = ",";
-
-
-struct record{
-  char** fields;
-  int num_fields;
-};
-
-
-struct record_table{
-  struct record** records;
-  int num_records;
-  int array_size;
-};
-
-
-struct record* parse_fields(char* buffer);
-struct record* empty_record();
-struct record_table* empty_record_table();
-void add_record(struct record_table* table, struct record* r);
-void resize_table(struct record_table* table, int new_size);
-void print_record_table(struct record_table* table);
-void print_record(struct record* r);
-void free_table(struct record_table* table);
-void free_record(struct record* r);
-
-
-int main(){
-  char buffer[MAXBUFFER];
-
-  char* file = "/home/ryan/CProjects/CSV/device_data.csv";
-  FILE* fptr = fopen(file, "r");
-
-  struct record_table* table = empty_record_table();
-  int i = 1;
-  while(fgets(buffer, MAXBUFFER, fptr) != NULL){
-    struct record* r = parse_fields(buffer);
-    add_record(table, r);
-  }
-  //print_record(table->records[5]);
-  print_record_table(table);  
-  printf("OK.\n");
-
-  //Free all pointers that were malloc'd
-  fclose(fptr);
-  free_table(table);
-  
-  return 0;
-}
-
-
-struct record* parse_fields(char* buffer){
-  struct record* r = empty_record();
-  char* field = strtok(buffer, ",\n");
-  int i = 0;
-  while (field != NULL){
-    r->fields[i++] = strdup(field);
-    field = strtok(NULL, ",\n");
-  }
-  if (i == 0){
-    printf("Error: No fields read from buffer: %s\n", buffer);
-  }
-  r->num_fields = i;
-  return r;
-}
+static const int MAXBUFFER = 10000;
 
 
 struct record* empty_record(){
-  struct record* r = (struct record*) malloc(sizeof(struct record));
-  char** fields = (char**) malloc(MAXFIELDS*sizeof(char*));
+  struct record* r = malloc(sizeof(struct record));
+  char** fields = malloc(sizeof(char*));
   r->fields = fields;
   r->num_fields = 0;
+  r->array_size = 1;
   return r;
 }
 
 
-struct record_table* empty_record_table(){
-  struct record_table* table = (struct record_table*) malloc(sizeof(struct record_table));
-  struct record** records = (struct record**) malloc(50*sizeof(struct record*));
-  table->records = records;
-  table->num_records = 0;
-  table->array_size = 50;
-  return table;
-}
-
-
-void add_record(struct record_table* table, struct record* r){
-  if (table->array_size == table->num_records){
-    resize_table(table, 2*table->array_size);
-  }
-  table->records[table->num_records++] = r;
-}
-
-
-void resize_table(struct record_table* table, int new_size){
-  table->records = (struct record**) realloc(table->records, new_size*sizeof(struct record*));
-  table->array_size = new_size;
-}
-
-
-void print_record_table(struct record_table* table){
-  int i;
-  for (i=0; i<table->num_records; i++){
-    print_record(table->records[i]);
+void parse_fields(struct record* r, char* line, char* delimiters){
+  char* field;
+  field = strtok(line, delimiters);
+  while (field != NULL){
+    add_field(r, strdup(field));
+    field = strtok(NULL, delimiters);
   }
 }
+
+
+void add_field(struct record* r, char* field){
+  if (r->num_fields == r->array_size){
+    int new_size = 2*r->array_size;
+    r->fields = realloc(r->fields, new_size*sizeof(char*));
+    r->array_size = new_size;
+  }
+  r->fields[r->num_fields++] = field;
+}
+
 
 void print_record(struct record* r){
   int i;
+  char end_character;
   for (i=0; i<r->num_fields; i++){
-    printf(" %s ", r->fields[i]);
+    end_character = (i==r->num_fields-1) ? '\n' : ',';
+    printf("%s%c", r->fields[i], end_character);
   }
-  printf("\n");
 }
 
-void free_table(struct record_table* table){
-  int i;
-  for (i=0; i<table->num_records; i++){
-    free_record(table->records[i]);
-  }
-  free(table);
-}
 
 void free_record(struct record* r){
   int i;
   for (i=0; i<r->num_fields; i++){
     free(r->fields[i]);
   }
+  free(r->fields);
   free(r);
 }
+
+
+struct table* empty_table(){
+  struct table* t = malloc(sizeof(struct table));
+  struct record** records = malloc(sizeof(struct record*));
+  t->records = records;
+  t->num_records = 0;
+  t->array_size = 1;
+  return t;
+}
+
+
+struct table* read_csv(char* filename, char* delimiters){
+  FILE* fptr = fopen(filename, "r");
+  struct table* t = empty_table();
+  char buffer[MAXBUFFER];
+  
+  while (fgets(buffer, MAXBUFFER, fptr) != NULL){
+    struct record* r = empty_record();
+    parse_fields(r, buffer, delimiters);
+    add_record(t, r);
+  }
+
+  fclose(fptr);
+  return t;
+}
+
+
+void add_record(struct table* t, struct record* r){
+  if (t->num_records == t->array_size){
+    int new_size = 2*t->array_size;
+    t->records = realloc(t->records, new_size*sizeof(struct record*));
+    t->array_size = new_size;
+  }
+  t->records[t->num_records++] = r;
+}
+
+
+void print_table(struct table* t){
+  int i;
+  for (i=0; i<t->num_records; i++){
+    print_record(t->records[i]);
+  }
+}
+
+
+void free_table(struct table* t){
+  int i;
+  for (i=0; i<t->num_records; i++){
+    free_record(t->records[i]);
+  }
+  free(t->records);
+  free(t);
+}
+
+
